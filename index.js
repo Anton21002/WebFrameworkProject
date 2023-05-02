8bb5a936d1364e95caf39f41002cd6bb283697f6
Anton21002 <anton.satalin@student.hamk.fi>
2023-05-02 19:02:18 +0300

    Added pages for the Harsh pages
    Co-Authored-By: HarshChahal7982 <98454174+HarshChahal7982@users.noreply.github.com>

--- index.js
+++ index.js
@@ -91,6 +91,95 @@ app.get('/login', (req, res) => {
         });
 });
 
+// end of anton pages
+//Start of Harsh pages
+
+app.get('/clothes', async (req, res) => {
+    try {
+        const clothes = await Clothe.find();
+        res.render('list-clothes', {
+            title: 'Clothe List',
+            clothes: clothes.map(clothe => clothe.toJSON())
+        });
+    } catch (error) {
+        res.status(404).json({
+            msg: "Clothe not found"
+        });
+    }
+});
+
+// Get a single clothe by ID
+app.get('/clothes/:id', async (req, res) => {
+    try {
+        const clothe = await Clothe.findById(req.params.id);
+        res.render('show-clothe', {
+            title: 'Clothe Details',
+            clothe: clothe.toJSON()
+        });
+    } catch (error) {
+        res.status(404).json({
+            msg: "Clothe not found"
+        });
+    }
+});
+
+// Display the form to add a new clothe
+app.get('/clothes/new', (req, res) => {
+    res.render('add-clothe');
+});
+
+// Create a new clothe
+app.post('/clothes', async (req, res) => {
+    try {
+        const clothe = new Clothe(req.body);
+        await clothe.save();
+
+        res.redirect('/clothes');
+    } catch (error) {
+        res.status(404).json({
+            msg: "Clothe not added"
+        });
+    }
+});
+
+// Display the form to edit a clothe
+app.get('/clothes/:id/edit', async (req, res) => {
+    try {
+        const clothe = await Clothe.findById(req.params.id);
+        res.render('edit-clothe', {
+            title: 'Edit Clothe',
+            clothe: clothe.toJSON()
+        });
+    } catch (error) {
+        res.status(404).json({
+            msg: "Clothe not found"
+        });
+    }
+});
+
+// Update a clothe
+app.put('/clothes/:id', async (req, res) => {
+    try {
+        await Clothe.findByIdAndUpdate(req.params.id, req.body);
+        res.redirect(`/clothes/${req.params.id}`);
+    } catch (error) {
+        res.status(404).json({
+            msg: "Clothe not updated"
+        });
+    }
+});
+
+// Delete a clothe
+app.delete('/clothes/:id', async (req, res) => {
+    try {
+        await Clothe.findByIdAndDelete(req.params.id);
+        res.redirect('/clothes');
+    } catch (error) {
+        res.status(404).json({
+            msg: "Clothe not deleted"
+        });
+    }
+});
 
 
 const PORT = process.env.PORT || 3000;
