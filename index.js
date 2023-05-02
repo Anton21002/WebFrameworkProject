5293af84844d710d33e4b031c6a66dcb1e89bf5b
Anton21002 <anton.satalin@student.hamk.fi>
2023-05-02 18:02:30 +0300

    Added images and Orges css
    Co-Authored-By: Orges Gashi <97589473+orgesgashi@users.noreply.github.com>

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set('view engine', 'handlebars');
 
 //Backup Mock Database
 //Retrieve local JSON backup database 
-//let items = require('C:\\Users\\Anton\\Documents\\GitHub\\WebFrameworkProject\\backupitem.json');
+let items = require('C:\\Users\\Anton\\Documents\\GitHub\\WebFrameworkProject\\backupitem.json');
 //console.log(items);
 //In-code database
 /*let items = [{id: 1,
@@ -90,6 +90,7 @@ app.get('/login', (req, res) => {
             desc : "Login",
         });
 });
+
 
 
 const PORT = process.env.PORT || 3000;
