4b581ba3c3a0d55666766b9aae3e3d60d6999714
Orges Gashi <97589473+orgesgashi@users.noreply.github.com>
2023-05-04 12:00:09 +0300

    deleted commented lines

--- index-header-footer.js
+++ index-header-footer.js
@@ -1,5 +1,3 @@
-//const http = require('http');
-//const fs = require('fs');
 const path = require('path');
 const nodemailer = require('nodemailer');
 const express = require('express');
@@ -57,45 +55,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
-
-
-
-
-/*
-const server = http.createServer((req, res) => {
-  let filePath = '.' + req.url;
-  if (filePath == './') {
-    filePath = './login.html';
-  }
-
-  const extname = path.extname(filePath);
-  let contentType = 'text/html';
-  switch (extname) {
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-  }
-
-  fs.readFile(filePath, function(error, content) {
-    if (error) {
-      if (error.code == 'ENOENT') {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('File not found');
-      } else {
-        res.writeHead(500);
-        res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-      }
-    } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf-8');
-    }
-  });
-});
-
-server.listen(3000);
-console.log('Server running at http://localhost:3000/');
-*/
