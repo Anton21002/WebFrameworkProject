99e1d55b251bd0821da20e3ed90cdf862a558e08
Orges Gashi <97589473+orgesgashi@users.noreply.github.com>
2023-05-04 11:35:17 +0300

    new changes for error handling

--- server.js
+++ server.js
@@ -1,39 +1,57 @@
-const http = require('http');
-const fs = require('fs');
 const path = require('path');
+const nodemailer = require('nodemailer');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-  let filePath = '.' + req.url;
-  if (filePath == './') {
-    filePath = './login.html';
+const hbs = require("hbs")
+const app = express();
+const port = 3000;
+
+const tempelatePath = path.join(__dirname, '../views')
+
+app.set("view engine", "hbs")
+app.set("views", tempelatePath)
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+const transporter = nodemailer.createTransport({
+  service: 'hotmail',
+  auth: {
+    user: 'orgesgashi41@outlook.com',
+    pass: '<password>' /*actual password*/
+  },
+  tls: {
+    rejectUnauthorized: false
   }
+});
 
-  const extname = path.extname(filePath);
-  let contentType = 'text/html';
-  switch (extname) {
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-  }
-
-  fs.readFile(filePath, function(error, content) {
+app.post('/send', (req, res) => {
+  const email = req.body.email;
+  const mailOptions = {
+    from: 'orgesgashi41@outlook.com',
+    to: email,
+    subject: 'Your subscription to StreetWear',
+    text: 'Welcome to our newsletter! Thank you for subscribing to our NewsLetter! Now you will be up to date with everything from now on :)'
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      if (error.code == 'ENOENT') {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('File not found');
-      } else {
-        res.writeHead(500);
-        res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-      }
+      console.log(error);
+      res.send('Error: Something went wrong. Please try again later.');
     } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf-8');
+      console.log('Email sent: ' + info.response);
+      res.render("login", {msg: 'You have been subscribed to our newsletter!'});
     }
   });
 });
 
-server.listen(3000);
-console.log('Server running at http://localhost:3000/');
+
+app.get('/', (req, res) => {
+  res.render("login")
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
